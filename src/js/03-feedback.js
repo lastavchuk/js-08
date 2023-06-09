331565d2bbe3bd1de47e99f4948b4efac7144c11
lastavchuk <it.lastavchuk@gmail.com>
2023-06-09 20:50:41 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,12 +3,11 @@ import debounce from 'lodash.debounce';
 const form = document.querySelector('.feedback-form');
 const KEYWORLD = 'feedback-form-state';
 
+let rezObj = {};
 updateForm();
 
 form.addEventListener('input', debounce(onInputChange, 500));
 form.addEventListener('submit', onSubmit);
-
-const rezObj = {};
 
 function onInputChange(event) {
   rezObj[event.target.name] = event.target.value;
@@ -20,6 +19,7 @@ function onSubmit(event) {
   event.preventDefault();
 
   console.log(rezObj);
+  rezObj = {};
   localStorage.removeItem(KEYWORLD);
 
   form.reset();
@@ -31,8 +31,10 @@ function updateForm() {
     try {
       obj = JSON.parse(obj);
       const { email, message } = obj;
-      form.email.value = email || '';
-      form.message.value = message || '';
+      rezObj.email = email || '';
+      rezObj.message = message || '';
+      form.email.value = rezObj.email;
+      form.message.value = rezObj.message;
     } catch (error) {
       console.log(`Error: ${error.name}.\nMessage: ${error.message}`);
     }
